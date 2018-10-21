cd1219eeb753739626305f50d4671264fdfe0365
abhijeetmane2312 <abhijeetmane2312@gmail.com>
2018-10-21 12:20:44 +0530

    Added code for login functionality

--- src/test/java/com/technocredits/orangeHRM/TestScripts/LoginTest.java
+++ src/test/java/com/technocredits/orangeHRM/TestScripts/LoginTest.java
@@ -1,5 +1,18 @@
 package com.technocredits.orangeHRM.TestScripts;
 
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+
+//This is the test case layer
+// All common actions related to login functionality are present in CommonTest class so we extends this class and pass the login credentiaLs
 public class LoginTest extends CommonTest {
+	
+	@Test
+	public void doLogin() throws Exception
+	{
+		login("admin", "admin123");
+	}
 
 }
